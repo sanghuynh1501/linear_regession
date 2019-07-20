88d9a3501d35bfad1749d84c55713d144101d99c
sanghuynh1501 <sanghuynh1501@gmail.com>
2019-07-20 09:29:42 +0700

    update

--- javascript_version.js
+++ javascript_version.js
@@ -3,9 +3,10 @@ const fs = require('fs')
 const accounting = require('accounting')
 
 function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
+    return 0;
 }  
 
+//Đọc file csv
 function read_csv(file_link) {
     let x_vectors = []
     let y_vectors = []
@@ -37,11 +38,11 @@ function read_csv(file_link) {
                         max_year = item.salePrice
                     }
                     let x_vector = [
-                        // parseFloat(item.square) / 9986,
-                        // parseFloat(streets.indexOf(item.street)),
-                        // parseFloat(neighborhoods.indexOf(item.neighborhood)),
+                        parseFloat(item.square) / 9986,
+                        parseFloat(streets.indexOf(item.street)),
+                        parseFloat(neighborhoods.indexOf(item.neighborhood)),
                         parseFloat(item.yearBuilt) / 2009,
-                        // parseFloat(item.yearSold) / 2010,
+                        parseFloat(item.yearSold) / 2010,
                     ]
                     let y_vector = [
                         parseFloat(item.salePrice / 99900)
@@ -57,6 +58,7 @@ function read_csv(file_link) {
     })
 }
 
+// Đọc dữ liệu tập train và test từ file csv
 async function read_data(data_path) {
     let train = await read_csv(data_path + '/train.csv')
     let test = await read_csv(data_path + '/test.csv')
@@ -66,6 +68,7 @@ async function read_data(data_path) {
     }
 }
 
+// Hàm nhân ma trận: A[m, n] * B[n, p] = C[m, p]
 function multiply(a, b) {
     var aNumRows = a.length, aNumCols = a[0].length,
         bNumRows = b.length, bNumCols = b[0].length,
@@ -73,7 +76,7 @@ function multiply(a, b) {
     for (var r = 0; r < aNumRows; ++r) {
       m[r] = new Array(bNumCols) // initialize the current row
       for (var c = 0; c < bNumCols; ++c) {
-        m[r][c] = 0;             // initialize the current cell
+        m[r][c] = 0;
         for (var i = 0; i < aNumCols; ++i) {
           m[r][c] += a[r][i] * b[i][c]
         }
@@ -82,48 +85,92 @@ function multiply(a, b) {
     return m
 }
 
+// hàm cộng ma trận: A[m, n] + B[m, n] = C[m, n]
 function add(a, b) {
     var res = []
     a.forEach((t, n1) => {
         let sums = []
         t.forEach((num, n2) => {
-        sums.push(num + b[n1][n2])
+            sums.push(num + b[n1][n2])
         });
         res.push(sums)
     })
     return res
 }
 
+// hàm trừ ma trận: A[m, n] - B[m, n] = C[m, n]
 function minus(a, b) {
     var res = []
     a.forEach((t, n1) => {
         let sums = []
         t.forEach((num, n2) => {
-        sums.push(num - b[n1][n2])
+            sums.push(num - b[n1][n2])
         });
         res.push(sums)
     })
     return res
 }
 
+// hàm nhân ma trận với một số: number * A
+function dot(number, x) {
+  for (let i = 0; i < x.length; i++) {
+      for (let j = 0; j < x[i].length; j++) {
+          x[i][j] = x[i][j] * number
+      }
+  }
+  return x
+}
+
+// Hàm tính ma trận chuyển vị: A^T
+function trainport(x) {
+  // Calculate the width and height of the Array
+  var a = x,
+  w = a.length ? a.length : 0,
+  h = a[0] instanceof Array ? a[0].length : 0;
+
+ // In case it is a zero matrix, no transpose routine needed.
+ if (h === 0 || w === 0) {
+     return [];
+ }
+
+ var i, j, t = [];
+
+ // Loop through every item in the outer array (height)
+ for (i = 0; i < h; i++) {
+
+     // Insert a new row (array)
+     t[i] = [];
+
+     // Loop through every item per item in outer array (width)
+     for (j = 0; j < w; j++) {
+
+         // Save transposed data.
+         t[i][j] = a[j][i];
+     }
+ }
+ return t
+}
+
+// hàm bình phương ma trận
 function square(x) {
     return multiply(x, x)
 }
 
+// hàm khởi tạo giá trị ngẫu nhiên cho weight và bias
 function random_matrix(r, c) {
     let matrix = []
     for (let i = 0; i < r; i++) {
         let row = []
         for (let j = 0; j < c; j++) {
-            row.push(Math.random())
+            row.push(0)
         }
         matrix.push(row)
     }
     return matrix
 }
 
+// phương trình linear_regession: Y = X * W + b
 function linear_regession(x, w, b) {
-    // x * w + b
     let dupplicate_b = []
     for (i = 0; i < x.length; i++) {
         dupplicate_b.push(b[0])
@@ -131,90 +178,52 @@ function linear_regession(x, w, b) {
     return add(multiply(x, w), dupplicate_b)
 }
 
+// hàm mất mát: (Y_pred - (W * X + b)) ^ 2
 function loss_function(x, y, w, b) {
     let loss = 0
     for (let i = 0; i < x.length; i++) {
-        loss += Math.abs(minus([y[i]], linear_regession([x[i]], w, b))[0][0])
+        loss += square(minus([y[i]], linear_regession([x[i]], w, b)))[0][0]
     }
     return loss / (x.length * 2)
 }
 
-function dot(number, x) {
-    for (let i = 0; i < x.length; i++) {
-        for (let j = 0; j < x[i].length; j++) {
-            x[i][j] = x[i][j] * number
-        }
-    }
-    return x
-}
-
-function trainport(x) {
-     // Calculate the width and height of the Array
-     var a = x,
-     w = a.length ? a.length : 0,
-     h = a[0] instanceof Array ? a[0].length : 0;
-
-    // In case it is a zero matrix, no transpose routine needed.
-    if (h === 0 || w === 0) {
-        return [];
-    }
-
-    var i, j, t = [];
-
-    // Loop through every item in the outer array (height)
-    for (i = 0; i < h; i++) {
-
-        // Insert a new row (array)
-        t[i] = [];
-
-        // Loop through every item per item in outer array (width)
-        for (j = 0; j < w; j++) {
-
-            // Save transposed data.
-            t[i][j] = a[j][i];
-        }
-    }
-    return t
-}
-
+// đạo hàm của weight: 1/N * X^T * (X * W - Y)
 function grad_weight (x, y, w, b) {
     let N = x.length
     return dot(1/N, multiply(trainport(x), minus(multiply(x, w), y)))
 }
 
+// đạo hàm của bias: 1/N * X^T * (X * W + B)
 function grad_bias (x, y, w, b) {
     let N = x.length
     return dot(1/N, linear_regession(x, y, w, b))
 }
 
+// tối ưu giá trị weight bằng gradient_descent: W = W - learning_rate * grad_weight
 function gradient_descent_weight(weight, train, weight, bias, learning_rate) {
     return minus(weight, dot(learning_rate, grad_weight (train.x, train.y, weight, bias)))
 }
 
+// tối ưu gía trị bias bằng gradient_descent: Bias = Bias - learning_rate * grad_bias
 function gradient_descent_bias(weight, train, weight, bias, learning_rate) {
     return minus(bias, dot(learning_rate, grad_bias (train.x, train.y, weight, bias)))
 }
 
 async function main() {
     let { train, test } = await read_data('./data')
-    let weight = random_matrix(1, 1)
+    let weight = random_matrix(5, 1)
     let bias = random_matrix(1, 1)
     let learning_rate = 0.002
-    let dem = 0
+    let epoch = 0
     loss = loss_function(train.x, train.y, weight, bias)
     console.log('loss ', loss)
-    while (dem < 10000) {
+    while (epoch < 10000) {
         weight = gradient_descent_weight(weight, train, weight, bias, learning_rate)
         bias = gradient_descent_bias(weight, train, weight, bias, learning_rate)
         loss = loss_function(train.x, train.y, weight, bias)
-        console.log(loss)
-        dem++
+        console.log('epoch ' + epoch + ': loss ', loss)
+        epoch++
     }
-
-    // for(let i = 0; i < test.x.length; i++) {
-    //     let y_pred = linear_regession([test.x[i]], weight, bias)
-    //     console.log('y: ' + accounting.format(test.y[i] * 99900) + ', y_pred: ' + accounting.format(y_pred * 99900))
-    // }
 }
 
 main()
--- tensorflow_version.py
+++ tensorflow_version.py
@@ -2,68 +2,74 @@ import tensorflow as tf
 import pandas as pd
 import numpy as np
 
+# Đọc csv
 def read_csv(data_path):
     data = pd.read_csv(data_path)
     return data.values
 
+# hàm linear regession: Y = W * X + B
 def prediction(x, weight, bias):
     return weight*x + bias # our predicted (learned) m and c, expression is like y = m*x + c
 
+# hàm mất mát: (Y_pred - (W * X + b)) ^ 2
 def loss(x, y, weights, biases): 
     error = prediction(x, weights, biases) - y # how 'wrong' our predicted (learned) y is
     squared_error = tf.square(error)
     return tf.reduce_mean(input_tensor=squared_error) # overall mean of squared error, scalar value.
 
+# đạo hàm của weight: 1/N * X^T * (X * W - Y)
 def grad_weight(x, y, weights, biases):
     with tf.GradientTape() as tape:
         tape.watch(weights)
         loss_ = loss(x, y, weights, biases)
     return tape.gradient(loss_, weights) # direction and value of the gradient of our weights and biases
 
+# đạo hàm của bias: 1/N * X^T * (X * W + B)
 def grad_bias(x, y, weights, biases):
     with tf.GradientTape() as tape:
         tape.watch(biases)
         loss_ = loss(x, y, weights, biases)
     return tape.gradient(loss_, biases) # direction and value of the gradient of our weights and biases
 
-streets = ['Pave', 'Grvl']
-neighborhoods = [
-    'CollgCr', 'Veenker', 'Crawfor',
-    'NoRidge', 'Mitchel', 'Somerst',
-    'NWAmes',  'OldTown', 'BrkSide',
-    'Sawyer',  'NridgHt', 'NAmes',
-    'SawyerW', 'IDOTRR',  'MeadowV',
-    'Edwards', 'Timber',  'Gilbert',
-    'StoneBr', 'ClearCr', 'NPkVill',
-    'Blmngtn', 'BrDale',  'SWISU',
-    'Blueste'
-]
+def load_data():
+    data = read_csv("data/train.csv")
+    streets = ['Pave', 'Grvl']
+    neighborhoods = [
+        'CollgCr', 'Veenker', 'Crawfor',
+        'NoRidge', 'Mitchel', 'Somerst',
+        'NWAmes',  'OldTown', 'BrkSide',
+        'Sawyer',  'NridgHt', 'NAmes',
+        'SawyerW', 'IDOTRR',  'MeadowV',
+        'Edwards', 'Timber',  'Gilbert',
+        'StoneBr', 'ClearCr', 'NPkVill',
+        'Blmngtn', 'BrDale',  'SWISU',
+        'Blueste'
+    ]
+    x_train = []
+    y_train = []
+    for i in range(len(data)):
+        x = []
+        y = []
+        x.append(float(data[i][1]) / 9986)
+        if data[i][2] in streets:
+            x.append(float(streets.index(data[i][2])) / 2)
+        else:
+            x.append(1)
+        x.append(float(neighborhoods.index(data[i][3])) / len(neighborhoods))
+        x.append(float(data[i][4]) / 2009)
+        x.append(float(data[i][5]) / 2010)
+        x_train.append(x)
+        y.append(float(data[i][6]) / 99900)
+        y_train.append(y)
+    x_train = np.array(x_train)
+    y_train = np.array(y_train)
+    return x_train, y_train
 
-data = read_csv("data/train.csv")
-x_train = []
-y_train = []
-for i in range(len(data)):
-    x = []
-    y = []
-    print('data[i] ', data[i])
-    x.append(float(data[i][1]) / 9986)
-    if data[i][2] in streets:
-        x.append(float(streets.index(data[i][2])) / 2)
-    else:
-        x.append(1)
-    x.append(float(neighborhoods.index(data[i][3])) / len(neighborhoods))
-    x.append(float(data[i][4]) / 2009)
-    x.append(float(data[i][5]) / 2010)
-    x_train.append(x)
-    y.append(float(data[i][6]) / 99900)
-    y_train.append(y)
-x_train = np.array(x_train)
-y_train = np.array(y_train)
-print("np.random.randn() ", np.random.randn())
+x_train, y_train = load_data()
 W = tf.Variable(np.random.randn()) # initial, random, value for predicted weight (m)
 B = tf.Variable(np.random.randn()) # initial, random, value for predicted bias (c)
 learning_rate = 0.01
-print("Initial loss: {:.3f}".format(loss(x, y, W, B)))
+print("Initial loss: ", loss(x, y, W, B))
 for step in range(3000): #iterate for each training step
     deltaW = grad_weight(x_train, y_train, W, B)
     deltaB = grad_bias(x_train, y_train, W, B) # direction(sign) and value of the gradients of our loss 
